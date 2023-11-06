39ca2a2264fc04f7f6e8ace3b5371133f2fcc003
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2023-11-07 01:07:52 +0700

    Project Finish

---
+++ app/src/main/java/com/example/esjumbo/Data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.esjumbo.Data
+
+import com.example.esjumbo.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.original,
+        R.string.vanila,
+        R.string.kacangijo,
+        R.string.anggur,
+        R.string.lyche
+    )
+}
